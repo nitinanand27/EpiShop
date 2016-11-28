583eb0f217779523c7f0ccaa378cb17cedd262ba
AI1C3\dearfriends <nitin.anand@dearfriends.se>
2016-11-28 12:59:49 +0100

    Add project files.

---
+++ Epishop/Business/IModifyLayout.cs
@@ -0,0 +1,13 @@
+using Epishop.Models.ViewModels;
+
+namespace Epishop.Business
+{
+    /// <summary>
+    /// Defines a method which may be invoked by PageContextActionFilter allowing controllers
+    /// to modify common layout properties of the view model.
+    /// </summary>
+    interface IModifyLayout
+    {
+        void ModifyLayout(LayoutModel layoutModel);
+    }
+}
