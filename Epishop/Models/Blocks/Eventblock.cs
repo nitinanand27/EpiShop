afc7165eb726c8032c31de3dae36d52b48080d24
AI1C3\dearfriends <nitin.anand@dearfriends.se>
2016-12-07 13:25:48 +0100

    Events page & event block created

---
+++ Epishop/Models/Blocks/Eventblock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace Epishop.Models.Blocks
+{
+    [ContentType(DisplayName = "Eventblock", GUID = "3ecb6f70-c745-4d69-8100-12c90fffaa43", Description = "a block to display event")]
+    public class Eventblock : BlockData
+    {
+        [Display(Name = "Event Name", Order = 1, GroupName = SystemTabNames.Content)]
+        public virtual string Name { get; set; }
+
+        [Display(Name = "Location", Description = "Where event will take place", Order = 2, GroupName = SystemTabNames.Content)]
+        public virtual string Place { get; set; }
+
+        [Display(Name = "Date Time", Description = "when event will take place", Order = 3, GroupName = SystemTabNames.Content)]
+        public virtual DateTime DateTime { get; set; }
+
+        [Display(Name = "Event Image", Description = "image picture", Order = 4, GroupName = SystemTabNames.Content)]
+        public virtual ContentReference Image { get; set; }
+
+    }
+}
--- Epishop/Models/Pages/EventPage.cs
+++ Epishop/Models/Pages/EventPage.cs
@@ -1,23 +1,20 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
-using EPiServer.Core;
+﻿using EPiServer.Core;
 using EPiServer.DataAbstraction;
 using EPiServer.DataAnnotations;
-using EPiServer.SpecializedProperties;
+using Epishop.Models.Blocks;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Epishop.Models.Pages
 {
     [ContentType(DisplayName = "EventPage", GUID = "5ab72018-aac4-4149-9c7c-640351e2e240", Description = "")]
+    [ImageUrl("~/Epishop/Epishop/Static/img/event-tent-icon-camping-tent.jpg")]
     public class EventPage : PageData
     {
-        /*
-                [CultureSpecific]
-                [Display(
-                    Name = "Main body",
-                    Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
-                    GroupName = SystemTabNames.Content,
-                    Order = 1)]
-                public virtual XhtmlString MainBody { get; set; }
-         */
+        [Display(Name ="Heading Text", GroupName =SystemTabNames.Content, Order =1)]
+        public virtual string HeadingText { get; set; }
+
+        [Display(Name = "Events",Description ="Event blocks for every event", GroupName = SystemTabNames.Content, Order = 2)]
+        public virtual ContentArea Eventblock { get; set; }
     }
 }
